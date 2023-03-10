1537dc5d7d8591fdd08d4e3c36f102cf03652eaf
likalika24 <lika.benjusite@tktk.ee>
2023-03-10 18:33:54 +0200

    fix movie year

--- scripts/app.js
+++ scripts/app.js
@@ -1,14 +1,19 @@
 const container = document.querySelector('.container');
 const movieTitle = document.querySelector('.userInputTitle');
 const moviePosterUrl = document.querySelector('.userInputPosterUrl');
+const movieYearOfPublication = document.querySelector('.userInputYear');
 const btn = document.querySelector('.button');
 const movieTitleToDisplay = document.querySelector('.favoriteMovieTitle');
+const movieYearToDisplay = document.querySelector('.favoriteMovieYear');
 
 let titleInStorage = localStorage.getItem('title');
 let imageUrlInStorage = localStorage.getItem('imageUrl');
+let yearInStorage = localStorage.getItem('year');
 
-if(titleInStorage && imageUrlInStorage) {
+
+if(titleInStorage && imageUrlInStorage && yearInStorage) {
     movieTitleToDisplay.textContent = titleInStorage;
+    movieYearToDisplay.textContent = yearInStorage;
     container.style.backgroundImage = `linear-gradient(rgb(51, 51, 82, 0.685), rgb(73, 49, 49, 0.582)), 
     url('${imageUrlInStorage}')`;
 }
@@ -17,11 +22,16 @@ if(titleInStorage && imageUrlInStorage) {
 btn.addEventListener('click', () => {
     let movieTitleInput = movieTitle.value;
     let posterUrlInput = moviePosterUrl.value;
+    let movieYearInput = movieYearOfPublication.value;
     localStorage.setItem('title', movieTitleInput);
     localStorage.setItem('imageUrl', posterUrlInput);
+    localStorage.setItem('year', movieYearInput);
     movieTitleToDisplay.textContent = movieTitleInput;
+    movieYearToDisplay.textContent = movieYearInput;
     container.style.backgroundImage = `linear-gradient(rgb(51, 51, 82, 0.685), rgb(73, 49, 49, 0.582)), 
     url('${posterUrlInput}')`;
     movieTitle.value = '';
     moviePosterUrl.value = '';
+    movieYearOfPublication.value = '';
+
 });
