564921800075c053c9ee355b7a885ca031f08b8f
likalika24 <lika.benjusite@tktk.ee>
2023-03-10 16:39:29 +0200

    background url

---
+++ scripts/app.js
@@ -0,0 +1,27 @@
+const container = document.querySelector('.container');
+const movieTitle = document.querySelector('.userInputTitle');
+const moviePosterUrl = document.querySelector('.userInputPosterUrl');
+const btn = document.querySelector('.button');
+const movieTitleToDisplay = document.querySelector('.favoriteMovieTitle');
+
+let titleInStorage = localStorage.getItem('title');
+let imageUrlInStorage = localStorage.getItem('imageUrl');
+
+if(titleInStorage && imageUrlInStorage) {
+    movieTitleToDisplay.textContent = titleInStorage;
+    container.style.backgroundImage = `linear-gradient(rgb(51, 51, 82, 0.685), rgb(73, 49, 49, 0.582)), 
+    url('${imageUrlInStorage}')`;
+}
+
+
+btn.addEventListener('click', () => {
+    let movieTitleInput = movieTitle.value;
+    let posterUrlInput = moviePosterUrl.value;
+    localStorage.setItem('title', movieTitleInput);
+    localStorage.setItem('imageUrl', posterUrlInput);
+    movieTitleToDisplay.textContent = movieTitleInput;
+    container.style.backgroundImage = `linear-gradient(rgb(51, 51, 82, 0.685), rgb(73, 49, 49, 0.582)), 
+    url('${posterUrlInput}')`;
+    movieTitle.value = '';
+    moviePosterUrl.value = '';
+});
